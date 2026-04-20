170ba0ac945ea137c881a49906431edb979fd2ee
Sofia Sannino <sofia.sannino@gmail.com>
2026-04-20 18:12:43 +0200

    finalize shape generation

--- data_generation/generate.py
+++ data_generation/generate.py
@@ -11,7 +11,9 @@ class Circle():
     def __init__(self, translation: tuple, scale: float, num_points=100):
         self.radius = 1.0
         self.center = (0.0, 0.0)
-        self.translate(translation)
+        dx = translation[0]
+        dy = translation[1]
+        self.translate(dx, dy)
         self.scale(scale)
         if num_points is not None:
             self.points = self.compute_points(num_points)
@@ -51,12 +53,18 @@ class Hexagon(Circle):
         points_per_edge = num_points // 6
         x = np.array([])
         y = np.array([])
-        for x_i, y_i in zip(xx, yy):
-            for j in np.linspace(x_i, xx[(np.where(xx == x_i)[0] + 1) % 6], points_per_edge, endpoint=False):
-                x = np.append(x, j)
-                y = np.append(y, np.interp(j, [x_i, xx[(np.where(xx == x_i)[0] + 1) % 6]], [y_i, yy[(np.where(yy == y_i)[0] + 1) % 6]]))
+        for i in range(6):
+            x_i, y_i = xx[i], yy[i]
+            x1, y1 = xx[(i + 1) % 6], yy[(i + 1) % 6]
 
-        return x, y
+            t = np.linspace(0, 1, points_per_edge, endpoint=False)
+            x_edge = x_i + t * (x1 - x_i)
+            y_edge = y_i + t * (y1 - y_i)
+
+            x = np.append(x, x_edge)
+            y = np.append(y, y_edge)
+
+        return np.array(x), np.array(y)
 
 class Triangle(Circle):
     def __init__(self, angle: float, translation: tuple, scale: float, num_points=100):
@@ -73,10 +81,16 @@ class Triangle(Circle):
         points_per_edge = num_points // 3
         x = np.array([])
         y = np.array([])
-        for x_i, y_i in zip(xx, yy):
-            for j in np.linspace(x_i, xx[(np.where(xx == x_i)[0] + 1) % 3], points_per_edge, endpoint=False):
-                x = np.append(x, j)
-                y = np.append(y, np.interp(j, [x_i, xx[(np.where(xx == x_i)[0] + 1) % 3]], [y_i, yy[(np.where(yy == y_i)[0] + 1) % 3]]))
+        for i in range(3):
+            x_i, y_i = xx[i], yy[i]
+            x1, y1 = xx[(i + 1) % 3], yy[(i + 1) % 3]
+
+            t = np.linspace(0, 1, points_per_edge, endpoint=False)
+            x_edge = x_i + t * (x1 - x_i)
+            y_edge = y_i + t * (y1 - y_i)
+
+            x = np.append(x, x_edge)
+            y = np.append(y, y_edge)
 
         return x, y
         
@@ -115,33 +129,44 @@ class GenerateShape():
             coords = np.stack((x, y), axis=-1)
             V.append(coords) # vertices of shapes
             L.append(self.connectivity(V)) # connectivity of the shapes
+        return V, L
 
     
     def connectivity(self, V):
         L = []
-        for i in range(len(V)):
-            L.append((i, (i + 1) % len(V)))  # connect each vertex to the next and the last to the first
+        l_list = []
+        for v in V:
+            for i in range(len(v)):
+                l_list.append((i, (i + 1) % len(v)))  # connect each vertex to the next and the last to the first
+            L.append(l_list)
         return L
            
-@hydra.main(config_path="/home/sofiasannino/projects/rl-mesh-deformation/configs", config_name="generate")
+@hydra.main(version_base=None, config_path="/home/sofiasannino/projects/rl-mesh-deformation/configs", config_name="generate")
 def main(cfg: DictConfig):
+    print("Generating shapes with the following configuration:")
+    print(cfg)
     shapes ={
-        'name': 'circle', 'num_points': 100,
-        'name': 'hexagon', 'num_points': 100,
-        'name': 'triangle', 'num_points': 100
+        'circle': 60,
+        'hexagon': 60,
+        'triangle': 60
     }
     shape_generator = GenerateShape(shapes, cfg)
-    shape_generator.generate_shapes()
-
-    for V, L in zip(shape_generator.V, shape_generator.L):
+    V, L = shape_generator.generate_shapes()
+    print("Shapes generated successfully!")
+    i = 0 
+    for v, l in zip(V, L):
         plt.figure()
-        plt.scatter(V[:, 0], V[:, 1])
-        for edge in L:
-            plt.plot(V[edge, 0], V[edge, 1], 'k-')
+        plt.scatter(v[:, 0], v[:, 1])
+        for edge in l:
+            plt.plot(v[edge, 0], v[edge, 1], 'k-')
         plt.title('Generated Shape')
         plt.axis('equal')
         plt.show()
-        plt.savefig(f"{cfg.output_dir}/generated_shape.png")
+        plt.savefig(f"generated_shape_{i}.png")
+        i += 1
+
+
+if __name__ == "__main__":    main()
 
 
    
